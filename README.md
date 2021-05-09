b0d2197d4dfaa476194408ac8406b388490a7a7d
Baavgai <Baavgai@chaingang.org>
2021-05-09 04:27:54 -0400

    examples added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # InfiniteGridTS
+
 Created with CodeSandbox
+
+Then with `yarn create react-app my-app --template typescript` because that template didn't want to play nice outside the sandbox.
+
+Some code adapted from https://codesandbox.io/s/fervent-darwin-i8ssl.
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,56 @@
-import React, { createRef, Fragment, PureComponent } from "react";
-import { FixedSizeGrid as Grid, GridChildComponentProps } from "react-window";
-import InfiniteLoader from "react-window-infinite-loader";
+import React, { useState } from 'react';
+import { Layout } from "./Layout"
+import { InfiniteItemExample } from "./InfiniteItemExample";
+import { InfiniteGridExample } from "./InfiniteGridExample";
 
-const NUM_COLUMNS = 3;
 
-let itemStatusMap: { [index: number]: boolean } = {};
+type ExampleType = "ex1" | "ex2";
 
-const isItemLoaded = (index: number) => !!itemStatusMap[index];
+interface AppState {
+  selected: ExampleType;
+}
 
-const loadMoreItems = (startIndex: number, stopIndex: number) => {
-  for (let index = startIndex; index <= stopIndex; index++) {
-    itemStatusMap[index] = false;
+interface AppController {
+  selectExample: (x: ExampleType) => void;
+}
+
+type AppProps = AppState & AppController;
+
+const useAppProps = (): AppProps => {
+  const [state, setState] = useState<AppState>({ selected: "ex1" });
+  const selectExample = (selected: ExampleType) => setState({ selected });
+  return { ...state, selectExample };
+}
+
+const ExampleView = (p: AppState) => {
+  switch (p.selected) {
+    case "ex1":
+      return <InfiniteItemExample />;
+    case "ex2":
+      return <InfiniteGridExample />;
+    default:
+      return <>Oops</>;
   }
-  return new Promise<void>(resolve =>
-    setTimeout(() => {
-      for (let index = startIndex; index <= stopIndex; index++) {
-        itemStatusMap[index] = true;
-      }
-      resolve();
-    }, 2500)
-  );
 };
 
-const Cell = ({ columnIndex, rowIndex, style }: GridChildComponentProps) => {
-  let label;
-  const itemIndex = rowIndex * NUM_COLUMNS + columnIndex;
-  if (itemStatusMap[itemIndex] === true) {
-    label = `Cell (${rowIndex}, ${columnIndex})`;
-  } else {
-    label = "Loading...";
-  }
-  return (
-    <div className="ListItem" style={style}>
-      {label}
-    </div>
-  );
-};
+const Choose = (p: AppProps) =>
+  <select className="form-select" onChange={x => p.selectExample(x.target.value as any)} value={p.selected}>
+    <option value="ex1">Example 1</option>
+    <option value="ex2">Example 2</option>
+  </select>;
 
+const ContentView = (p: AppProps) =>
+  <div className="row">
+    <div className="col-3"><Choose {...p} /></div>
+    <div className="col"><ExampleView {...p} /></div>
+  </div>;
+
+const Content = () => {
+  const p = useAppProps();
+  return <ContentView {...p} />;
+};
 
 export const App = () =>
-  <>
-    <p className="Note">
-      This demo app mimics loading remote data with a 2.5s timer. While rows
-      are "loading" they will display a "Loading..." label. Once data has been
-      "loaded" the row number will be displayed. Start scrolling the list to
-      automatically load data.
-      </p>
-    <InfiniteLoader
-      isItemLoaded={isItemLoaded}
-      itemCount={1000}
-      loadMoreItems={loadMoreItems}
-    >
-      {({ onItemsRendered, ref }) => (
-        <Grid
-          className="List"
-          columnCount={NUM_COLUMNS}
-          columnWidth={100}
-          height={150}
-          rowCount={1000}
-          rowHeight={35}
-          onItemsRendered={gridProps => {
-            onItemsRendered({
-              overscanStartIndex:
-                gridProps.overscanRowStartIndex * NUM_COLUMNS,
-              overscanStopIndex: gridProps.overscanRowStopIndex * NUM_COLUMNS,
-              visibleStartIndex: gridProps.visibleRowStartIndex * NUM_COLUMNS,
-              visibleStopIndex: gridProps.visibleRowStopIndex * NUM_COLUMNS
-            });
-          }}
-          ref={ref}
-          width={300}
-        >
-          {Cell}
-        </Grid>
-      )}
-    </InfiniteLoader>
-    <p className="Note">
-      Check out the documentation to learn more:
-        <br />
-      <a href="https://github.com/bvaughn/react-window-infinite-loader#documentation">
-        github.com/bvaughn/react-window-infinite-loader
-        </a>
-    </p>
-  </>;
+  <Layout>
+    <Content />
+  </Layout>;
